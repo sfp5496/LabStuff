a0c57ba16a4440b56dd2a728611d678667679982
sfp5496 <sfp5496@g.rit.edu>
2017-01-02 17:23:40 -0500

    SC.h

---
+++ SC.h
@@ -0,0 +1,22 @@
+#ifndef __SC_H__
+#define __SC_H__
+
+#define npart 1024
+
+#define LENGTH 7.0 //Max x-distance, doesn't do anything
+#define HEIGHT 7.0 //Max y-distance, doesn't do anything
+#define WIDTH 7.0 //Max z-distance, doesn't do anything
+
+#define R 4.5, Radius of container
+#define H 1.4, Height of container
+
+#define PHI 1.0 //Maximum packing fraction
+#define ALPHA 3.0 // l/sigma, aspect ratio
+
+#define SIGMA 0.0 //initial rod diameter
+#define L 0.0 //initial rod length
+
+#define DT .05 //Timestep, doesn't do anything
+#define DR .025 //Max displacement in one dimension per timestep, doesn't do anything
+
+#endif
